8425411ce8e6bffad59c70673a8669af6dffa0c9
jakubjereczek <58139870+jakubjereczek@users.noreply.github.com>
2020-08-25 10:10:21 +0200

    save data in localStorage

--- List.js
+++ List.js
@@ -40,6 +40,7 @@ class List {
     }
     async render(elementy = this.tasks.tasksList) {
         this.ul.textContent = "";
+        localStorage.removeItem(`data`);
 
         console.log('Wielkosc tablicy ' + elementy.length);
 
@@ -67,21 +68,36 @@ class List {
             this.ul.appendChild(newElement);
 
         })
+        localStorage.setItem(`data`, JSON.stringify(this.tasks.getElements()));
+
         this.counter.textContent = this.tasks.getLenght();
 
     }
     // load elements from tasks.json 
     init() {
-        fetch('./tasks.json')
-            .then(response => response.json())
-            .then(tasks => {
-                tasks.forEach(task => {
-                    this.tasks.addTask(task.key, task.text, task.status, task.type);
-                    this.render(); // docelowo w innym miejscu, ale metoda jest asynchroniczna wyzej
-                })
+        // fetch('./tasks.json')
+        //     .then(response => response.json())
+        //     .then(tasks => {
+        //         localStorage.setItem(`data`, JSON.stringify(tasks));
 
-            })
-            .catch(error => console.log(error));
+        //         tasks.forEach(task => {
+        //             this.tasks.addTask(task.key, task.text, task.status, task.type);
+        //             // dodanie ich do pamięci przeglądarki
+        //             this.render(); // docelowo w innym miejscu, ale metoda jest asynchroniczna wyzej
+        //         })
+        //     })
+        //     .catch(error => console.log(error));
+
+
+        // localStorage - ładowanie danych z przeglądarki
+        const data = JSON.parse(localStorage.getItem('data'));
+        console.log(data);
+        data.forEach(task => {
+            this.tasks.addTask(task.id, task.value, task.status, task.type);
+            if (this.tasks.getLenght() === data.length) {
+                this.render();
+            }
+        })
     }
 
 }
